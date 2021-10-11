f4053d965dc2fd434f92e60b2f9fee60c9c4b4a4
Hacı Çakın <haci.cakin@ug.bilkent.edu.tr>
2021-10-11 17:22:08 +0300

    backend project is created

---
+++ bilboardBackend/src/main/java/com/jack_the_coder/bilboard_backend/BilboardBackendApplication.java
@@ -0,0 +1,18 @@
+package com.jack_the_coder.bilboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Hacı Çakın
+ * @created 11.10.2021
+ * @update 11.10.2021
+ */
+@SpringBootApplication
+public class BilboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilboardBackendApplication.class, args);
+	}
+
+}
---
+++ bilboardBackend/src/test/java/com/jack_the_coder/bilboard_backend/BilboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack_the_coder.bilboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

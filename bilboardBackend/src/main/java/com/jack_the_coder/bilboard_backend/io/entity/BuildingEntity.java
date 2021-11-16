38a5503bfc2703e632e9b00e4b472d3082fb3232
aslidinc <asli.dinc@ug.bilkent.edu.tr>
2021-11-16 17:09:22 +0300

    BuildingEntity added, UniversityEntity compeleted

--- bilboardBackend/src/main/java/com/jack_the_coder/bilboard_backend/io/entity/BuildingEntity.java
+++ bilboardBackend/src/main/java/com/jack_the_coder/bilboard_backend/io/entity/BuildingEntity.java
@@ -1,4 +1,31 @@
 package com.jack_the_coder.bilboard_backend.io.entity;
 
-public class BuildingEntity {
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * @author Aslı Dinç
+ * @apiNote This class is base structure of the buildings in database
+ * @implNote After completing relations, getters and setters should be implemented
+ * @since 11.11.2021
+ */
+@Entity( name = "buildings" )
+public class BuildingEntity implements Serializable {
+
+    private static final long serialVersionUID = -3993541896569285225L;
+
+    @GeneratedValue
+    @Id
+    @Column( name = "id" )
+    private long id;
+
+    private String name;
+
+    @ManyToOne( targetEntity = UniversityEntity.class )
+    @JoinColumn( name = "university" )
+    private UniversityEntity university;
+
+    // todo one-to-many classrooms
+
 }

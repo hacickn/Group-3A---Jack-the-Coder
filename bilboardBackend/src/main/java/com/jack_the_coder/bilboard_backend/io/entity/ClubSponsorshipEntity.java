0df75ba592b618a1a0f8cd232f7e0b56e98f8a88
aslidinc <asli.dinc@ug.bilkent.edu.tr>
2021-11-16 22:24:31 +0300

    ClubSponsorshipEntity getter and setters added

--- bilboardBackend/src/main/java/com/jack_the_coder/bilboard_backend/io/entity/ClubSponsorshipEntity.java
+++ bilboardBackend/src/main/java/com/jack_the_coder/bilboard_backend/io/entity/ClubSponsorshipEntity.java
@@ -36,5 +36,99 @@ public class ClubSponsorshipEntity implements Serializable {
     @JoinColumn( name = "club" )
     private ClubEntity club;
 
+    /**
+     * Get id method
+     * @return long
+     */
+    public long getId () {
+        return id;
+    }
 
+    /**
+     * Set id method
+     * @param id is an long parameter
+     */
+    public void setId ( long id ) {
+        this.id = id;
+    }
+
+    /**
+     * Get name method
+     * @return String
+     */
+    public String getName () {
+        return name;
+    }
+
+    /**
+     * Set name method
+     * @param name is String
+     */
+    public void setName ( String name ) {
+        this.name = name;
+    }
+
+    /**
+     * Get photo method
+     * @return String
+     */
+    public String getPhoto () {
+        return photo;
+    }
+
+    /**
+     * Set photo method
+     * @param photo is String
+     */
+    public void setPhoto ( String photo ) {
+        this.photo = photo;
+    }
+
+    /**
+     * Get amount method
+     * @return int
+     */
+    public int getAmount () {
+        return amount;
+    }
+
+    /**
+     * Set amount method
+     * @param amount is int
+     */
+    public void setAmount ( int amount ) {
+        this.amount = amount;
+    }
+
+    /**
+     * Get type method
+     * @return String
+     */
+    public String getType () {
+        return type;
+    }
+
+    /**
+     * Set photo method
+     * @param type is String
+     */
+    public void setType ( String type ) {
+        this.type = type;
+    }
+
+    /**
+     * Get club method
+     * @return ClubEntity
+     */
+    public ClubEntity getClub () {
+        return club;
+    }
+
+    /**
+     * Set club method
+     * @param club is ClubEntity
+     */
+    public void setClub ( ClubEntity club ) {
+        this.club = club;
+    }
 }

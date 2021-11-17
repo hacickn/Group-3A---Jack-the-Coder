71a673990544688be98b648c76b06814c07085e8
Metehan Saçakçı <metehan.sacakci@ug.bilkent.edu.tr>
2021-11-17 20:36:30 +0300

    RoomRequest Component and Screen are edited
    RoomRequest Component and Screen are edited
    Metehan Saçakçı

---
+++ bilboard-frontend/src/components/BilboardRoomRequest.js
@@ -0,0 +1,50 @@
+import {Grid} from "@mui/material";
+import Colors from "../utils/Colors";
+import Constants from "../utils/Constants";
+import BilboardButton from "./BilboardButton";
+
+/**
+ * Bilboard Room Request
+ * 
+ * Metehan Saçakçı
+ * 16.11.2021
+ */
+
+const BilboardRoomRequest = (props) => {
+
+    return (<Grid container 
+        style={{
+            marginBottom: 5,
+            marginTop: 5,
+            paddingTop: 20,
+            paddingBottom: 20,
+            borderRadius: Constants.BORDER_RADIUS,
+            background: Colors.BILBOARD_BLUE_ALTERNATIVE,
+            alignItems: "center",
+        }}>
+        
+            <Grid item xs>
+                <img src={props.roomRequest.photo}
+                    style={{maxHeight: 100, maxWidth: 100}} />                      
+            </Grid>
+
+            <Grid item xs>
+                {props.roomRequest.eventName}                     
+            </Grid>
+
+            <Grid item xs>
+                No of participants: {props.roomRequest.numberOfParticipants}                     
+            </Grid>
+
+            <Grid item xs>
+                Preferred Room: {props.roomRequest.prefferedRoom}                     
+            </Grid>
+
+            <Grid item xs>
+                <BilboardButton width="100px" fontSize="11px" text="Assign Room"/>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default BilboardRoomRequest;
--- bilboard-frontend/src/screens/RoomRequestsScreen.js
+++ bilboard-frontend/src/screens/RoomRequestsScreen.js
@@ -1,5 +1,7 @@
 import {Grid} from "@mui/material";
 import BilboardNavbar from "../components/BilboardNavbar";
+import BilboardRoomRequest from "../components/BilboardRoomRequest";
+import Constants from "../utils/Constants";
 
 /**
  * Room Requests Screen
@@ -8,10 +10,105 @@ import BilboardNavbar from "../components/BilboardNavbar";
  * 16.11.2021
  */
 
-import BilboardRoomRequests from "../components/BilboardRoomRequests";
-import Constants from "../utils/Constants";
-
 const RoomRequestScreen = (props) => {
+
+    const roomRequestList = [
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "40",
+            prefferedRoom: "BZ-02",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "50",
+            prefferedRoom: "BZ-03",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "60",
+            prefferedRoom: "BZ-04",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "70",
+            prefferedRoom: "BZ-05",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "80",
+            prefferedRoom: "BZ-06",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "90",
+            prefferedRoom: "BZ-07",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "100",
+            prefferedRoom: "BZ-08",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "100",
+            prefferedRoom: "BZ-08",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "100",
+            prefferedRoom: "BZ-08",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "100",
+            prefferedRoom: "BZ-08",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "100",
+            prefferedRoom: "BZ-08",
+        },
+
+        {
+            photo: "https://upload.wikimedia.org//wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png",
+            eventName: "kariyer.net example event",
+            numberOfParticipants: "100",
+            prefferedRoom: "BZ-08",
+        },
+    ]
+
+    function addRoomRequest( roomRequestObject)
+    {
+        let temp = [...roomRequestList]
+
+        temp.push({
+            photo: roomRequestObject.photo,
+            eventName: roomRequestObject.eventName,
+            numberOfParticipants: roomRequestObject.numberOfParticipants,
+            prefferedRoom: roomRequestObject.numberOfParticipants,
+        })
+    }
 
     return (
         <Grid container
@@ -29,9 +126,11 @@ const RoomRequestScreen = (props) => {
             }} item xs={12}>
                 Room Requests
             </Grid>
-            <BilboardRoomRequests/>
+
+            <Grid container style={{maxHeight: "100vh", overflowY: "scroll", marginLeft: 15, marginRight: 10}}>
+                {roomRequestList.map( roomRequest => <BilboardRoomRequest roomRequest={roomRequest}/> )}
+            </Grid>
         </Grid>
     )
 }
-
 export default RoomRequestScreen;

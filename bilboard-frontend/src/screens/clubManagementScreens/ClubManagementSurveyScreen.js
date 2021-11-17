15e688541ccd93299b1b860211a18635b5dced15
Metehan Saçakçı <metehan.sacakci@ug.bilkent.edu.tr>
2021-11-17 11:34:35 +0300

    Component and Screen are added
    - Bilboard Survey Screen Cards component is added
    - Club Management Survey Screen is added
    Metehan Saçakçı

--- bilboard-frontend/src/screens/clubManagementScreens/ClubManagementSurveyScreen.js
+++ bilboard-frontend/src/screens/clubManagementScreens/ClubManagementSurveyScreen.js
@@ -1,5 +1,42 @@
+import {Grid} from "@mui/material";
+import Constants from "../../utils/Constants";
+import BilboardSurveyCards from "../../components/BilboardSurveyCards";
+import BilboardButton from "../../components/BilboardButton";
+
+/**
+ * Club Management Survey Screen
+ * 
+ * Metehan Saçakçı
+ * 17.11.2021
+ */
+
 const ClubManagementSurveyScreen = () => {
-    return <div> ClubManagementSponsorScreen</div>
+
+
+    return (
+        <Grid container>
+            <Grid style={{
+                marginTop: 15,
+                fontFamily: Constants.OXYGEN_FONT_FAMILY,
+                fontSize: 48,
+                paddingBottom: 20,
+            }} item xs={12}>
+                Active Surveys
+            </Grid>
+
+            <Grid item xs={12}>
+                <BilboardSurveyCards/>
+            </Grid>
+
+            <Grid item xs= {12} style={{
+                marginTop: 50,
+            }}>
+                <BilboardButton width="200px" fontSize="20px" text="Add Survey"/>
+            </Grid>
+
+        </Grid>
+
+    )
 }
 
 export default ClubManagementSurveyScreen;

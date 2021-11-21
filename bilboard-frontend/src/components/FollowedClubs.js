497fc40bcae8ed190a89866631b2d4ed632964c2
Hacı Çakın <haci.cakin@ug.bilkent.edu.tr>
2021-11-21 11:30:19 +0300

    user screen is updated

--- bilboard-frontend/src/components/FollowedClubs.js
+++ bilboard-frontend/src/components/FollowedClubs.js
@@ -1,64 +1,65 @@
 import { makeStyles } from "@mui/styles";
 import BilboardButton from "./BilboardButton";
 import Grid from "@mui/material/Grid";
+import Colors from "../utils/Colors";
 
-const useStyles = makeStyles({
-  root: {
-    minHeight: "100px",
-    borderRadius: "10px",
-    background: "#E0F2F1",
-  },
-  clubLogo: {
-    width: "300px",
-    marginTop: "10px",
-    marginLeft: "20px",
-    height: "80px",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-  },
-});
+const useStyles = makeStyles( {
+    root: {
+        minHeight: "100px",
+        borderRadius: "10px",
+        background: "#E0F2F1",
+    },
+    clubLogo: {
+        width: "300px",
+        marginTop: "10px",
+        marginLeft: "20px",
+        height: "80px",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+    },
+} );
 
-const FollowedClubs = (props) => {
-  const classes = useStyles();
-  return (
-    <div
-      className={classes.root}
-      style={{ maxWidth:"95%", marginTop: "30px" }}
-    >
-      <Grid container>
-        <Grid item xs={6}>
-          <div
-            className={classes.clubLogo}
-            style={{ backgroundImage: `url(${props.image})` }}
-          ></div>
-        </Grid>
-        <Grid
-          item
-          xs={3}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "10px"
-          }}
+const FollowedClubs = ( props ) => {
+    const classes = useStyles();
+    return (
+        <div
+            className={ classes.root }
+            style={ { maxWidth: "100%", marginTop: "20px" } }
         >
-          <BilboardButton text="Go To Club Page" />
-        </Grid>
-        <Grid
-          item
-          xs={3}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "10px"
-          }}
-        >
-          <BilboardButton text="Leave Membership"/>
-        </Grid>
-      </Grid>
-    </div>
-  );
+            <Grid container>
+                <Grid item xs={ 6 }>
+                    <div
+                        className={ classes.clubLogo }
+                        style={ { backgroundImage: `url(${ props.image })` } }
+                    />
+                </Grid>
+                <Grid
+                    item
+                    xs={ 3 }
+                    style={ {
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        marginTop: "10px"
+                    } }
+                >
+                    <BilboardButton text="Go To Club Page" width={ "10vw" }/>
+                </Grid>
+                <Grid
+                    item
+                    xs={ 2 }
+                    style={ {
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        marginTop: "10px"
+                    } }
+                >
+                    <BilboardButton text="Leave Membership" width={ "10vw" } color={ Colors.BILBOARD_RED }/>
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
 
 export default FollowedClubs;
--- bilboard-frontend/src/screens/UserScreen.js
+++ bilboard-frontend/src/screens/UserScreen.js
@@ -7,120 +7,133 @@ import BilboardButton from "../components/BilboardButton";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
+import Colors from "../utils/Colors";
+import { Card } from "@mui/material";
 
-const useStyles = makeStyles({
-  root: {
-    height: "calc(100vh-68px)",
-  },
-  userImage: {
-    width: "200px",
-    height: "200px",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    borderRadius: "100px"
-  },
-});
+const useStyles = makeStyles( {
+    root: {
+        height: "calc(100vh-68px)",
+    },
+    userImage: {
+        width: "200px",
+        height: "200px",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        borderRadius: "100px"
+    },
+} );
 
-const UserScreen = (props) => {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <Grid container>
-        <Grid item xs={4}>
-          <Grid container direction="column">
-            <Grid item xs={4} style={{paddingTop: "40px",paddingBottom:"30px", display: "flex", justifyContent: "center", alignItems: "center"}}>
-              <div
-                className={classes.userImage}
-                style={{ backgroundImage: `url(${props.image})`}}
-              ></div>
+const UserScreen = ( props ) => {
+    const classes = useStyles();
+    return (
+        <div className={ classes.root }>
+            <Grid container>
+                <Grid item xs={ 4 }>
+                    <Card elevation={ 3 }
+                          style={ {
+                              borderRadius: Constants.BORDER_RADIUS,
+                              padding: "20px",
+                              margin: "40px",
+                              marginLeft: "100px",
+                              marginRight: "80px",
+
+                          } }>
+                        <Grid container direction="column">
+                            <Grid item xs={ 4 } style={ {
+                                paddingTop: "40px",
+                                paddingBottom: "30px",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            } }>
+                                <div
+                                    className={ classes.userImage }
+                                    style={ { backgroundImage: `url(${ props.image })` } }
+                                />
+                            </Grid>
+                            <Grid item xs={ 8 }>
+                                <Grid container>
+                                    <Grid
+                                        item
+                                        xs={ 12 }
+                                        style={ {
+                                            fontFamily: Constants.OXYGEN_FONT_FAMILY,
+                                            fontSize: "24px",
+                                        } }
+                                    >
+                                        Change Password
+                                    </Grid>
+                                    <Grid
+                                        item
+                                        xs={ 12 }
+                                        style={ { marginBottom: "10px", marginTop: "10px" } }
+                                    >
+                                        <BilboardTextField label="Current Password" width={ '280px' }/>
+                                    </Grid>
+                                    <Grid item xs={ 12 } style={ { marginBottom: "10px" } }>
+                                        <BilboardTextField label="New Password" width={ '280px' }/>
+                                    </Grid>
+                                    <Grid item xs={ 12 } style={ { marginBottom: "10px" } }>
+                                        <BilboardTextField label="Retype New Password" width={ '280px' }/>
+                                    </Grid>
+                                    <Grid item xs={ 12 }
+                                          style={ { marginBottom: "10px", display: "flex", justifyContent: "center" } }>
+                                        <FormGroup>
+                                            <FormControlLabel
+                                                control={ <Checkbox/> }
+                                                label="GE250/1"
+                                            />
+                                        </FormGroup>
+                                    </Grid>
+                                    <Grid item xs={ 12 } style={ { marginBottom: "10px" } }>
+                                        <BilboardButton text="Update" width={ '160px' } fontSize={ "12px" }/>
+                                    </Grid>
+
+                                    <Grid item xs={ 12 } style={ { marginTop: "10px" } }>
+                                        <BilboardButton color={ Colors.BILBOARD_RED } text="Sign Out" width="160px"
+                                                        fontSize="12px"/>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Card>
+                </Grid>
+                <Grid item xs={ 8 }>
+                    <Card elevation={ 3 } style={ {
+                        borderRadius: Constants.BORDER_RADIUS,
+                        padding: "20px",
+                        marginRight: "20px",
+                        marginTop:"40px"
+                    } }>
+                        <Grid container style={ { maxHeight: "75vh", overflowX: "scroll" } }>
+                            <Grid item xs={ 12 }>
+                                <p
+                                    style={ {
+                                        fontFamily: Constants.OXYGEN_FONT_FAMILY,
+                                        fontSize: "32px",
+                                        float: "left",
+                                    } }
+                                >
+                                    Followed Clubs
+                                </p>
+                            </Grid>
+                            <Grid item xs={ 12 }>
+                                <FollowedClubs image="https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png"/>
+                                <FollowedClubs image="https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png"/>
+                                <FollowedClubs image="https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png"/>
+                                <FollowedClubs image="https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png"/>
+                                <FollowedClubs image="https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png"/>
+                                <FollowedClubs image="https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png"/>
+                                <FollowedClubs image="https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png"/>
+                                <FollowedClubs image="https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png"/>
+                                <FollowedClubs image="https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/2/21/IEEE_logo.svg/1200px-IEEE_logo.svg.png"/>
+                            </Grid>
+                        </Grid>
+                    </Card>
+                </Grid>
             </Grid>
-            <Grid item xs={8}>
-              <Grid container>
-                <Grid
-                  item
-                  xs={12}
-                  style={{
-                    fontFamily: Constants.OXYGEN_FONT_FAMILY,
-                    fontSize: "24px",
-                  }}
-                >
-                  Change Password
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  style={{ marginBottom: "10px", marginTop: "10px" }}
-                >
-                  <BilboardTextField label="Current Password"/>
-                </Grid>
-                <Grid item xs={12} style={{ marginBottom: "10px" }}>
-                  <BilboardTextField label="New Password" />
-                </Grid>
-                <Grid item xs={12} style={{ marginBottom: "10px" }}>
-                  <BilboardTextField label="Retype New Password"/>
-                </Grid>
-                <Grid item xs={12} style={{ marginBottom: "30px" }}>
-                  <BilboardButton text="change" />
-                </Grid>
-                <Grid item xs={12}>
-                  <Grid container>
-                    <Grid
-                      item
-                      xs={6}
-                      style={{ display: "flex", justifyContent: "end" }}
-                    >
-                      <FormGroup>
-                        <FormControlLabel
-                          control={<Checkbox />}
-                          label="GE250/1"
-                        />
-                      </FormGroup>
-                    </Grid>
-                    <Grid item xs={1}></Grid>
-                    <Grid
-                      item
-                      xs={5}
-                      style={{ display: "flex", justifyContent: "start" }}
-                    >
-                      <BilboardButton text="update" />
-                    </Grid>
-                  </Grid>
-                </Grid>
-                <Grid item xs={12} style={{ marginTop: "30px" }}>
-                  <BilboardButton text="Sign Out" width="80px"  fontSize="14px"/>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={8}>
-          <Grid container>
-            <Grid item xs={12}>
-              <p
-                style={{
-                  fontFamily: Constants.OXYGEN_FONT_FAMILY,
-                  fontSize: "32px",
-                  float: "left",
-                }}
-              >
-                Followed Clubs
-              </p>
-            </Grid>
-            <Grid item xs={12}>
-              <FollowedClubs  image="https://picsum.photos/200"/>
-              <FollowedClubs  image="https://picsum.photos/200"/>
-              <FollowedClubs  image="https://picsum.photos/200"/>
-              <FollowedClubs  image="https://picsum.photos/200"/>
-              <FollowedClubs  image="https://picsum.photos/200"/>
-              <FollowedClubs  image="https://picsum.photos/200"/>
-              <FollowedClubs  image="https://picsum.photos/200"/>
-              <FollowedClubs  image="https://picsum.photos/200"/>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default UserScreen;

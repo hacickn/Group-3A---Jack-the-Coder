3632aa171488016f8a0a5f87441721adf752f208
abdullah-koc <abdullah.koc@ug.bilkent.edu.tr>
2021-12-21 22:08:41 +0300

    Managing membership screen was added + some improvements

--- bilboard-frontend/src/screens/ClubManagementScreen.js
+++ bilboard-frontend/src/screens/ClubManagementScreen.js
@@ -11,60 +11,98 @@ import ClubManagementSurveyScreen from "./clubManagementScreens/ClubManagementSu
 import ClubManagementFeedbackScreen from "./clubManagementScreens/ClubManagementFeedbackScreen";
 import ClubManagementMembersScreen from "./clubManagementScreens/ClubManagementMembersScreen";
 import ClubManagementMembershipRequestsScreen from "./clubManagementScreens/ClubManagementMembershipRequestsScreen";
-import {Card} from "@mui/material";
+import { Card } from "@mui/material";
 
 const ClubManagementScreen = () => {
+  const pages = [
+    "General",
+    "Add Events",
+    "Members",
+    "Membership Requests",
+    "Feedbacks",
+    "Surveys",
+    "Club Hierarchy",
+    "Sponsors",
+  ];
+  const [openedScreen, setOpenedScreen] = React.useState("General");
 
-    const pages = ["General", "Add Events", "Members", "Membership Requests", "Feedbacks", "Surveys", "Club Hierarchy", "Sponsors"];
-    const [openedScreen, setOpenedScreen] = React.useState("General")
-
-
-    return (<Grid container style={{padding: 20, height: "88vh"}}>
-        <Grid style={{
-            height: "85vh",
-            borderRadius: Constants.BORDER_RADIUS,
-            padding: 4,
+  return (
+    <Grid container style={{ padding: 20, height: "88vh" }}>
+      <Grid
+        style={{
+          height: "85vh",
+          borderRadius: Constants.BORDER_RADIUS,
+          padding: 4,
         }}
-              item xs={3}>
-            <Card elevation={4} style={{borderRadius: Constants.BORDER_RADIUS}}>
-                {pages.map(page => <Grid item xs={12} style={{
-                    paddingTop: 5,
-                    paddingBottom: 5,
-                }}>
-                    {openedScreen === page ?
-                        <BilboardButton text={page}
-                                        fontSize={16}
-                                        width="23vw"
-                                        height={"4vh"}
-                                        color={Colors.BILBOARD_MAIN}
-                                        textColor={Colors.WHITE}
-                        />
-                        :
-                        <BilboardButton text={page}
-                                        fontSize={16}
-                                        width="23vw"
-                                        height={"4vh"}
-                                        color={Colors.WHITE}
-                                        textColor={Colors.BILBOARD_MAIN}
-                                        onClick={() => setOpenedScreen(page)}
-                        />}
-                </Grid>)}
-            </Card>
-
-        </Grid>
-        <Grid item xs={9} style={{padding: 4}}>
-            <Card elevation={4} style={{borderRadius: Constants.BORDER_RADIUS, height: "85vh", overflowY: "scroll"}}>
-                {openedScreen === "General" ? <ClubManagementGeneralScreen/> :
-                    openedScreen === "Add Events" ? <ClubManagementAddEventScreen/> :
-                        openedScreen === "Members" ? <ClubManagementMembersScreen/> :
-                            openedScreen === "Membership Requests" ? <ClubManagementMembershipRequestsScreen/> :
-                                openedScreen === "Feedbacks" ? <ClubManagementFeedbackScreen/> :
-                                    openedScreen === "Surveys" ? <ClubManagementSurveyScreen/> :
-                                        openedScreen === "Club Hierarchy" ? <ClubManagementHierarchyScreen/> :
-                                            openedScreen === "Sponsors" ? <ClubManagementSponsorScreen/> : <div/>}
-            </Card>
-        </Grid>
-    </Grid>)
-}
+        item
+        xs={3}
+      >
+        <Card elevation={4} style={{ borderRadius: Constants.BORDER_RADIUS }}>
+          {pages.map((page) => (
+            <Grid
+              item
+              xs={12}
+              style={{
+                paddingTop: 5,
+                paddingBottom: 5,
+              }}
+            >
+              {openedScreen === page ? (
+                <BilboardButton
+                  text={page}
+                  fontSize={16}
+                  width="21vw"
+                  height={"9.36vh"}
+                  color={Colors.BILBOARD_MAIN}
+                  textColor={Colors.WHITE}
+                />
+              ) : (
+                <BilboardButton
+                  text={page}
+                  fontSize={16}
+                  width="21vw"
+                  height={"9.36vh"}
+                  color={Colors.WHITE}
+                  textColor={Colors.BILBOARD_MAIN}
+                  onClick={() => setOpenedScreen(page)}
+                />
+              )}
+            </Grid>
+          ))}
+        </Card>
+      </Grid>
+      <Grid item xs={9} style={{ padding: 4 }}>
+        <Card
+          elevation={4}
+          style={{
+            borderRadius: Constants.BORDER_RADIUS,
+            height: "85vh",
+            overflowY: "scroll",
+          }}
+        >
+          {openedScreen === "General" ? (
+            <ClubManagementGeneralScreen />
+          ) : openedScreen === "Add Events" ? (
+            <ClubManagementAddEventScreen />
+          ) : openedScreen === "Members" ? (
+            <ClubManagementMembersScreen />
+          ) : openedScreen === "Membership Requests" ? (
+            <ClubManagementMembershipRequestsScreen />
+          ) : openedScreen === "Feedbacks" ? (
+            <ClubManagementFeedbackScreen />
+          ) : openedScreen === "Surveys" ? (
+            <ClubManagementSurveyScreen />
+          ) : openedScreen === "Club Hierarchy" ? (
+            <ClubManagementHierarchyScreen />
+          ) : openedScreen === "Sponsors" ? (
+            <ClubManagementSponsorScreen />
+          ) : (
+            <div />
+          )}
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default ClubManagementScreen;
--- bilboard-frontend/src/screens/clubManagementScreens/ClubManagementMembershipRequestsScreen.js
+++ bilboard-frontend/src/screens/clubManagementScreens/ClubManagementMembershipRequestsScreen.js
@@ -1,9 +1,160 @@
+import Grid from "@mui/material/Grid";
+import Constants from "../../utils/Constants";
+import { makeStyles } from "@mui/styles";
+import RequestedMembership from "./clubManagementComponents/RequestedMembership";
+import PendingMembership from "./clubManagementComponents/PendingMembership";
+
+const useStyles = makeStyles({
+  container: {
+    height: "80vh",
+  },
+});
+
 const ClubManagementMembershipRequestsScreen = () => {
-    return (
-        <div>
-        <h1>Club Management Membership Requests Screen</h1>
-        </div>
-    );
-}
+  const classes = useStyles();
+  return (
+    <div>
+      <div style={{ fontSize: "36px", marginBottom: "10px" }}>
+        Membership Requests
+      </div>
+      <div style={{ fontSize: "18px", marginBottom: "10px" }}>
+        (A: Accept, P: Pending, R: Reject)
+      </div>
+      <Grid container>
+        <Grid item xs={6}>
+          <div className={classes.container}>
+            <div
+              style={{
+                fontSize: "24px",
+                fontWeight: "bolder",
+                fontFamily: Constants.OXYGEN_FONT_FAMILY,
+                marginBottom: "10px",
+              }}
+            >
+              Requests
+            </div>
+            <div>
+              <Grid container style={{ marginLeft: "2%" }}>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <RequestedMembership
+                    name="Aslı"
+                    surname="Dinç"
+                    ID="21802527"
+                  />
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={6}>
+          <div className={classes.container}>
+            <div
+              style={{
+                fontSize: "24px",
+                fontWeight: "bolder",
+                marginBottom: "10px",
+                fontFamily: Constants.OXYGEN_FONT_FAMILY,
+              }}
+            >
+              Pending
+            </div>
+            <div>
+              <Grid container style={{ marginLeft: "2%" }}>
+                <Grid item xs={12}>
+                  <PendingMembership name="Aslı" surname="Dinç" ID="21802527" />
+                  <PendingMembership name="Aslı" surname="Dinç" ID="21802527" />
+                  <PendingMembership name="Aslı" surname="Dinç" ID="21802527" />
+                  <PendingMembership name="Aslı" surname="Dinç" ID="21802527" />
+                  <PendingMembership name="Aslı" surname="Dinç" ID="21802527" />
+                  <PendingMembership name="Aslı" surname="Dinç" ID="21802527" />
+                  <PendingMembership name="Aslı" surname="Dinç" ID="21802527" />
+                  <PendingMembership name="Aslı" surname="Dinç" ID="21802527" />
+                  <PendingMembership name="Aslı" surname="Dinç" ID="21802527" />
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default ClubManagementMembershipRequestsScreen;

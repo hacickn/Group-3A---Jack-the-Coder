801360b6e3ae92b85192e370c2b34341c08d77b1
abdullah-koc <abdullah.koc@ug.bilkent.edu.tr>
2021-12-24 11:58:55 +0300

    Mail confirmation screen was completed and a dialog was added for confirming leaving membership of a club.

--- bilboard-frontend/src/components/FollowedClubs.js
+++ bilboard-frontend/src/components/FollowedClubs.js
@@ -2,75 +2,108 @@ import { makeStyles } from "@mui/styles";
 import BilboardButton from "./BilboardButton";
 import Grid from "@mui/material/Grid";
 import Colors from "../utils/Colors";
+import React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
-const useStyles = makeStyles( {
-    root: {
-        minHeight: "100px",
-        borderRadius: "10px",
-        background: Colors.BILBOARD_MAIN_ALTERNATIVE,
-    },
-    clubLogo: {
-        marginTop: "10px",
-        marginLeft: "20px",
-        height: "80px",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-    },
-} );
+const useStyles = makeStyles({
+  root: {
+    minHeight: "100px",
+    borderRadius: "10px",
+    background: Colors.BILBOARD_MAIN_ALTERNATIVE,
+  },
+  clubLogo: {
+    marginTop: "10px",
+    marginLeft: "20px",
+    height: "80px",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  },
+});
 
-const FollowedClubs = ( props ) => {
-    const classes = useStyles();
-    return (
-        <div
-            className={ classes.root }
-            style={ { maxWidth: "100%", marginTop: "20px" } }
+const FollowedClubs = (props) => {
+  const [isLeaveAlertOpen, setIsLeaveAlertOpen] = React.useState(false);
+
+  const handleLeaveMembership = () => {
+    setIsLeaveAlertOpen(true);
+  };
+
+  const handleCloseAlert = () => {
+    setIsLeaveAlertOpen(false);
+  };
+
+  const classes = useStyles();
+  return (
+    <div
+      className={classes.root}
+      style={{ maxWidth: "100%", marginTop: "20px" }}
+    >
+      <Grid container>
+        <Grid item xs={4}>
+          <div
+            className={classes.clubLogo}
+            style={ { backgroundImage: `url(${ process.env.REACT_APP_IMAGE_URL + props.club.photo })` } }
+          />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "10px",
+          }}
         >
-            <Grid container>
-                <Grid item xs={ 4 }>
-                    <div
-                        className={ classes.clubLogo }
-                        style={ { backgroundImage: `url(${ process.env.REACT_APP_IMAGE_URL + props.club.photo })` } }
-                    />
-                </Grid>
-                <Grid
-                    item
-                    xs={ 2 }
-                    style={ {
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        marginTop: "10px"
-                    } }
-                >
-                    {props.club.shortName.toUpperCase()}
-                </Grid>
-                <Grid
-                    item
-                    xs={ 3 }
-                    style={ {
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        marginTop: "10px"
-                    } }
-                >
-                    <BilboardButton text="Go To Club Page" width={ "10vw" }/>
-                </Grid>
-                <Grid
-                    item
-                    xs={ 3 }
-                    style={ {
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        marginTop: "10px"
-                    } }
-                >
-                    <BilboardButton text="Leave Membership" width={ "10vw" } color={ Colors.BILBOARD_RED }/>
-                </Grid>
-            </Grid>
-        </div>
-    );
+             {props.club.shortName.toUpperCase()}
+        </Grid>
+        <Grid
+          item
+          xs={3}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "10px",
+          }}
+        >
+          <BilboardButton text="Go To Club Page" width={"10vw"} />
+        </Grid>
+        <Grid
+          item
+          xs={3}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "10px",
+          }}
+        >
+          <BilboardButton
+            text="Leave Membership"
+            width={"10vw"}
+            color={Colors.BILBOARD_RED}
+            onClick={() => handleLeaveMembership()}
+          />
+        </Grid>
+      </Grid>
+      <Dialog open={isLeaveAlertOpen} onClose={handleCloseAlert}>
+        <DialogTitle>{"Leave Membership"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure to leave membership of the club {props.club.name}? 
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <BilboardButton onClick={handleCloseAlert} text="Cancel" />
+          <BilboardButton onClick={handleCloseAlert} text="Leave" autoFocus/>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 };
 
 export default FollowedClubs;
--- bilboard-frontend/src/screens/ConfirmationScreen.js
+++ bilboard-frontend/src/screens/ConfirmationScreen.js
@@ -1,6 +1,63 @@
 import React from "react";
+import BilboardButton from "../components/BilboardButton";
+import Grid from "@mui/material/Grid";
+import Constants from "../utils/Constants";
+import Colors from "../utils/Colors";
 
 export default function ConfirmationScreen() {
-
-    return ( <div> confirmation</div> )
+  const handleOnClick = () => {
+      
+  };
+  return (
+    <div style={{ height: "88vh", display: "flex", alignItems: "center" }}>
+      <Grid
+        container
+        style={{
+          margin: "20%",
+          padding: "80px 60px 80px 60px",
+          border: "5px solid",
+          borderRadius: "20px",
+          borderColor: Colors.BILBOARD_LIGHT_GREY,
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: "40px",
+          }}
+        >
+          <div
+            style={{
+              fontSize: "32px",
+              fontWeight: "bolder",
+              fontFamily: Constants.OXYGEN_FONT_FAMILY,
+            }}
+          >
+            Your account is successfully confirmed. Enjoy BilBoard!
+          </div>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <BilboardButton
+            text="Login"
+            color="#F44336"
+            fontSize="20px"
+            width="200px"
+            onClick={() => handleOnClick()}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }

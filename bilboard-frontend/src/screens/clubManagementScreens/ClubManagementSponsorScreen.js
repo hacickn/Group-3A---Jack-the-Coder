68b5275a24cd5c5eaef4d94213c4f1eb8461ff86
Metehan Saçakçı <metehan.sacakci@ug.bilkent.edu.tr>
2021-12-25 17:26:42 +0300

    ClubManagementSponsorScreen is updated
    ClubManagementSponsorScreen is updated
    Metehan Saçakçı

--- bilboard-frontend/src/screens/clubManagementScreens/ClubManagementSponsorScreen.js
+++ bilboard-frontend/src/screens/clubManagementScreens/ClubManagementSponsorScreen.js
@@ -44,6 +44,7 @@ const ClubManagementSponsorScreen = ( { club } ) => {
 
     if ( sponsorships.length === 0 && club.clubSponsorships.length !== 0 ) {
         setSponsorships( [ ...club.clubSponsorships ] )
+    }
 
     function handleSponsorshipAddition() {
         const formData = new FormData()
@@ -86,80 +87,6 @@ const ClubManagementSponsorScreen = ( { club } ) => {
         }
     }
 
-    const sponsorList = [
-        {
-            name: "Garanti",
-            amount: "10.000TL",
-            type: "https://www.pngall.com/wp-content/uploads/2017/03/Silver-Medal-Free-Download-PNG.png",
-            photo: "http://w3.bilkent.edu.tr/logo/ing-amblem.png"
-        },
-        {
-            name: "Google",
-            amount: "12.000TL",
-            type: "https://www.pngall.com/wp-content/uploads/2017/03/Silver-Medal-Free-Download-PNG.png",
-            photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/500px-Google_2015_logo.svg.png"
-        },
-        {
-            name: "Microsoft",
-            amount: "500.000TL",
-            type: "http://assets.stickpng.com/images/580b585b2edbce24c47b2af3.png",
-            photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Microsoft_logo_%282012%29.svg/2560px-Microsoft_logo_%282012%29.svg.png"
-        },
-        {
-            name: "Cyberpark",
-            amount: "1.000TL",
-            type: "https://www.diastar.com.tr/images/thumbnails/465/465/detailed/6/bronz_madalya_diastar.png",
-            photo: "https://www.cyberpark.com.tr/content/upload/settings/cyberparklogo-20190409135404.png"
-        },
-        {
-            name: "Mobiversite",
-            amount: "100.000TL",
-            type: "http://assets.stickpng.com/images/580b585b2edbce24c47b2af3.png",
-            photo: "https://static.wixstatic.com/media/c764a4_9979938f5f2447b1b0b94425b5d4cb99~mv2.png/v1/fill/w_512,h_512,al_c/c764a4_9979938f5f2447b1b0b94425b5d4cb99~mv2.png"
-        },
-        {
-            name: "Facebook",
-            amount: "10.000TL",
-            type: "https://www.pngall.com/wp-content/uploads/2017/03/Silver-Medal-Free-Download-PNG.png",
-            photo: "https://space-fun.com/wp-content/uploads/2018/08/logo-facebook-transparent.png"
-        },
-        {
-            name: "Garanti",
-            amount: "10.000TL",
-            type: "https://www.pngall.com/wp-content/uploads/2017/03/Silver-Medal-Free-Download-PNG.png",
-            photo: "http://w3.bilkent.edu.tr/logo/ing-amblem.png"
-        },
-        {
-            name: "Google",
-            amount: "12.000TL",
-            type: "https://www.pngall.com/wp-content/uploads/2017/03/Silver-Medal-Free-Download-PNG.png",
-            photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/500px-Google_2015_logo.svg.png"
-        },
-        {
-            name: "Microsoft",
-            amount: "500.000TL",
-            type: "http://assets.stickpng.com/images/580b585b2edbce24c47b2af3.png",
-            photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Microsoft_logo_%282012%29.svg/2560px-Microsoft_logo_%282012%29.svg.png"
-        },
-        {
-            name: "Cyberpark",
-            amount: "1.000TL",
-            type: "https://www.diastar.com.tr/images/thumbnails/465/465/detailed/6/bronz_madalya_diastar.png",
-            photo: "https://www.cyberpark.com.tr/content/upload/settings/cyberparklogo-20190409135404.png"
-        },
-        {
-            name: "Mobiversite",
-            amount: "100.000TL",
-            type: "http://assets.stickpng.com/images/580b585b2edbce24c47b2af3.png",
-            photo: "https://static.wixstatic.com/media/c764a4_9979938f5f2447b1b0b94425b5d4cb99~mv2.png/v1/fill/w_512,h_512,al_c/c764a4_9979938f5f2447b1b0b94425b5d4cb99~mv2.png"
-        },
-        {
-            name: "Facebook",
-            amount: "10.000TL",
-            type: "https://www.pngall.com/wp-content/uploads/2017/03/Silver-Medal-Free-Download-PNG.png",
-            photo: "https://space-fun.com/wp-content/uploads/2018/08/logo-facebook-transparent.png"
-        }
-    ]
 
     return <Grid container
                  style={ { margin: 0, padding: 10, borderRadius: Constants.BORDER_RADIUS } }>
@@ -223,6 +150,6 @@ const ClubManagementSponsorScreen = ( { club } ) => {
             </Grid>
         </Grid>
     </Grid>
-}
+};
 
 export default ClubManagementSponsorScreen;

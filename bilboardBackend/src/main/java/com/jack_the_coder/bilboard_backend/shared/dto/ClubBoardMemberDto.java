1448e426adcadd803be937105d4191cd575c51e6
Metehan Saçakçı <metehan.sacakci@ug.bilkent.edu.tr>
2021-12-26 06:13:40 +0300

    ClubBoardMemberDto is updated
    ClubBoardMemberDto is updated
    Metehan Saçakçı

--- bilboardBackend/src/main/java/com/jack_the_coder/bilboard_backend/shared/dto/ClubBoardMemberDto.java
+++ bilboardBackend/src/main/java/com/jack_the_coder/bilboard_backend/shared/dto/ClubBoardMemberDto.java
@@ -19,26 +19,50 @@ public class ClubBoardMemberDto implements Serializable {
     private UserEntity user;
     private ClubEntity club;
 
+    /**
+     * get id method
+     * @return id is a long
+     */
     public long getId () {
         return id;
     }
 
+    /**
+     * set id method
+     * @param id is a long parameter
+     */
     public void setId ( long id ) {
         this.id = id;
     }
 
+    /**
+     * get user method
+     * @return user is an UserEntity instance
+     */
     public UserEntity getUser () {
         return user;
     }
 
+    /**
+     * set user method
+     * @param user is a UserEntity instance parameter
+     */
     public void setUser ( UserEntity user ) {
         this.user = user;
     }
 
+    /**
+     * get club method
+     * @return club is a ClubEntity instance
+     */
     public ClubEntity getClub () {
         return club;
     }
 
+    /**
+     * set club method
+     * @param club is a ClubEntity instance parameter
+     */
     public void setClub ( ClubEntity club ) {
         this.club = club;
     }

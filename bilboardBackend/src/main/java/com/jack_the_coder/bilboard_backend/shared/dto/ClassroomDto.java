90e691aea52176a1686a4efab9464b9fa8befe3d
Metehan Saçakçı <metehan.sacakci@ug.bilkent.edu.tr>
2021-12-26 06:12:15 +0300

    ClassroomDto is updated
    ClassroomDto is updated
    Metehan Saçakçı

--- bilboardBackend/src/main/java/com/jack_the_coder/bilboard_backend/shared/dto/ClassroomDto.java
+++ bilboardBackend/src/main/java/com/jack_the_coder/bilboard_backend/shared/dto/ClassroomDto.java
@@ -22,43 +22,83 @@ public class ClassroomDto implements Serializable {
     private int capacity;
     private List<ClassroomDayEntity> classroomDays;
 
+    /**
+     * Get id method
+     * @return long
+     */
     public long getId () {
         return id;
     }
 
+    /**
+     * Set id method
+     * @param id is an long parameter
+     */
     public void setId ( long id ) {
         this.id = id;
     }
 
+    /**
+     * Get name method
+     * @return String
+     */
     public String getName () {
         return name;
     }
 
+    /**
+     * Set name method
+     * @param name is String
+     */
     public void setName ( String name ) {
         this.name = name;
     }
 
+    /**
+     * Get building method
+     * @return BuildingEntity object
+     */
     public BuildingEntity getBuilding () {
         return building;
     }
 
+    /**
+     * Set building method
+     * @param building is BuildingEntity instance
+     */
     public void setBuilding ( BuildingEntity building ) {
         this.building = building;
     }
 
+    /**
+     * Get classroom days method
+     * @return ClassroomDayEntity list
+     */
     public List<ClassroomDayEntity> getClassroomDays () {
         return classroomDays;
     }
 
+    /**
+     * Set classrooms days method
+     * @param classroomDays is ClassroomDayEntity list
+     */
     public void setClassroomDays (
             List<ClassroomDayEntity> classroomDays ) {
         this.classroomDays = classroomDays;
     }
 
+    /**
+     * get Capacity of class method
+     * @return capacity as an integer
+     */
     public int getCapacity () {
         return capacity;
     }
 
+    /**
+     * set capacity of class method
+     * @param capacity is an integer
+     */
     public void setCapacity ( int capacity ) {
         this.capacity = capacity;
     }
